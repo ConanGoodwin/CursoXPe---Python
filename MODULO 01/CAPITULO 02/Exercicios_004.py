376045afa3c080415efa5d3049db5c45bf2cc9f6
ConanGoodwin <conan_goodwin@hotmail.com>
2023-04-15 01:29:29 -0300

    feat: add exercicio 004 exemplos de tipos de erros em pyhton

---
+++ MODULO 01/CAPITULO 02/Exercicios_004.py
@@ -0,0 +1,27 @@
+# Erro de sintaxe
+x = 3
+y = 5
+print x + y
+
+
+# Erros de execução:
+
+# utilização de uma variável inexistente (não definida)
+x = 0
+print(xx)
+
+# declaração incorreta da variável dia, como Dia
+dia = 'Segunda Feira'
+print(Dia)
+
+# operação invalida
+x = 0
+25 / x
+
+
+# Erros logicos
+x = 5.0
+y = 7.0
+z = 12.0
+media = x + y + z / 3
+print(media)
