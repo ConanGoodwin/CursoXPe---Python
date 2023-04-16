9798c34b22360c265ac0dc9fba91090f186cd35b
ConanGoodwin <conan_goodwin@hotmail.com>
2023-04-16 02:38:28 -0300

    feat: add conteudo_002 capitulo 03, Operadores dos Tipos Primitivos de Dados

---
+++ MODULO 01/CAPITULO 03/conteudo_002.py
@@ -0,0 +1,16 @@
+x, y = 7, 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x // y)
+print(x**y)
+
+print(type(10 + 5))
+print(type(10 + 3.5))
+
+print(10 + False)  # Equivale a 10 + 0
+print(True + 5)  # Equivale a 1 + 5
+print(True + True + False)  # Equivale a 1 + 1 + 0
