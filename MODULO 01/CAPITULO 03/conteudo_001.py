f596e4e43ac41d9caa2476db332f6ceb3fed39df
ConanGoodwin <conan_goodwin@hotmail.com>
2023-04-16 02:34:54 -0300

    feat: add conteudo_001 capitulo 03, Tipos Primitivos de Dados em Python

---
+++ MODULO 01/CAPITULO 03/conteudo_001.py
@@ -0,0 +1,46 @@
+# tipo inteiros (int)
+x = 5
+y = -5
+
+print(type(x))
+
+# tipo float (float)
+x = 3.14
+y = -3.14
+
+print(type(x))
+
+
+# tipo boleano (bool)
+x = True
+y = False
+
+print(type(x))
+
+
+# tipo string (str)
+x = 'apas simples'
+y = "aspas duplas"
+z = '''aspas simples'''
+
+print(type(x))
+
+
+# tipo nulo ou NoneType (None)
+x = None
+y = None
+
+print(type(x))
+
+
+x = 'Este é um valor de string com "aspas" duplas dentro dela.'
+print(x)
+
+y = "Agora um valor de string com 'aspas' simples."
+print(y)
+
+z = '''Este é um valor de string
+declarado utilizando
+uma tripla de aspas que inclusive pode conter
+aspas 'simples' e "duplas" dentro dela.'''
+print(z)
